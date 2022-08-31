6f58b47924eaeb3884c453959b1063790d13e52b
Krishnapal Tomar <krishnapal.01tomar@gmail.com>
2022-08-31 18:54:21 +0530

    First commit initialize App to use TS

---
+++ ClientApp/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: {
+        site: './src/js/site.js'
+    },
+    output: {
+        filename: '[name].entry.js',
+        path: path.resolve(__dirname, '..', 'wwwroot', 'dist')
+    },
+    devtool: 'source-map',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader'],
+            },
+            {
+                test: /\.(eot|woff(2)?|ttf|otf|svg)$/i,
+                type: 'asset'
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        })
+    ]
+};
